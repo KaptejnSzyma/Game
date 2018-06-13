846c5fb201438e4ecab087aecceda51715bce863
Szymek <szymon.rzadca@gmail.com>
2018-06-13 16:07:28 +0200

    testing Troll function

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from enemy import Enemy, Troll
 
-random_monster = Enemy("Basic enemy", 12, 1)
-
-print(random_monster)
-
-random_monster.take_damage(4)
-print(random_monster)
-
-random_monster.take_damage(8)
-print(random_monster)
-
-random_monster.take_damage(9)
-print(random_monster)
-
 ugly_troll = Troll()
 
 print("Ugly troll - {}".format(ugly_troll))
+
+another_troll = Troll("Ug", 10, 1)
+print("Another troll - {}".format(another_troll))
+
+brother = Enemy("Urg", 23)
+print(brother)
