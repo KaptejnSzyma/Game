ab2cfd7dacff7d020053688df2e11dc497980d01
Szymek <szymon.rzadca@gmail.com>
2018-06-13 15:41:39 +0200

    adding necessary functions for Enemy class

--- enemy.py
+++ enemy.py
@@ -3,3 +3,14 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+
+    def take_damage(self, damage):
+        remaining_points = self.hit_points - damage
+        if remaining_points >= 0:
+            self.hit_points = remaining_points
+            print("I took {} points damage and have {} left").format(damage, self.hit_points)
+        else:
+            self.lives -= 1
+
+    def __str__(self):
+        return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
