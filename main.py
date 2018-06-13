2049725f5775a2f0e4d9de7952337532ba061f57
Szymek <szymon.rzadca@gmail.com>
2018-06-13 15:51:25 +0200

    testing enemy class

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from player import Player
-
-tim = Player("Tim")
-
-print(tim.name)
-print(tim.lives)
-
 from enemy import Enemy
 
 random_monster = Enemy("Basic enemy", 12, 1)
 
 print(random_monster)
-random_monster.take_damage(4)
 
+random_monster.take_damage(4)
+print(random_monster)
+
+random_monster.take_damage(8)
+print(random_monster)
+
+random_monster.take_damage(9)
 print(random_monster)
